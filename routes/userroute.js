b8269f948d72e06e2b3dca22f2308c2c8d12a487
kusumasri-23 <161683093+kusumasri-23@users.noreply.github.com>
2024-10-10 21:20:34 +0530

    Update userroute.js

--- routes/userroute.js
+++ routes/userroute.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/usermodel");
+router.post("/reg", async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const user = new User({ username, email, password });
+        await user.save();
+        res.status(201).send({ message: "Registration successful!" });
+    } catch (error) {
+        console.error("Error saving user:", error);
+        res.status(500).send({ message: "Error saving user", error: error.message });
+    }
+});
+
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
+    if (!user) {
+        return res.status(400).send({ message: "Invalid username or password" });
+    }
+    res.status(200).send({ message: "Login successful!" });
+});
+module.exports = router;
 
